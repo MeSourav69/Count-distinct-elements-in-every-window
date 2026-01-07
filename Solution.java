a1911c837fd23258e9424cd3fc3e2a817781dcc7
SOURAV RAY <souravray288@gmail.com>
2026-01-07 22:27:33 +0530

    Implement method to count distinct elements in subarrays

---
+++ Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    ArrayList<Integer> countDistinct(int arr[], int k) {
+        ArrayList<Integer> res = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < k; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        res.add(map.size());
+        
+        for (int i = k; i < arr.length; i++) {
+            int out = arr[i - k];
+            map.put(out, map.get(out) - 1);
+            if (map.get(out) == 0) map.remove(out);
+            
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            res.add(map.size());
+        }
+        
+        return res;
+    }
+}
